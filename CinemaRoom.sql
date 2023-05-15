8ae9dd6e6f938bdd7396dd1dbae52327022dfdec
Excel Chendrawan <92035860+XcelCh@users.noreply.github.com>
2023-05-15 23:29:43 +0800

    Add files via upload

--- CinemaRoom.sql
+++ CinemaRoom.sql
@@ -14,24 +14,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE CinemaRoom (
   ID int(3) PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+  Name VARCHAR(25) NOT NULL,
   Capacity int(3) NOT NULL,
   State VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO CinemaRoom (ID, Capacity, State)
-VALUES (1, 150, "Available");
+INSERT INTO CinemaRoom (Name, Capacity, State)
+VALUES ("Snowdrop", 150, "Available");
 
-INSERT INTO CinemaRoom (ID, Capacity, State)
-VALUES (2, 200, "Not Available");
+INSERT INTO CinemaRoom (Name, Capacity, State)
+VALUES ("Amaryllis", 200, "Not Available (Renovation)");
 
-INSERT INTO CinemaRoom (ID, Capacity, State)
-VALUES (3, 125, "Available");
+INSERT INTO CinemaRoom (Name, Capacity, State)
+VALUES ("Alchemilla", 125, "Available");
 
-INSERT INTO CinemaRoom (ID, Capacity, State)
-VALUES (4, 175, "Available");
+INSERT INTO CinemaRoom (Name, Capacity, State)
+VALUES ("Iris", 175, "Available");
 
-INSERT INTO CinemaRoom (ID, Capacity, State)
-VALUES (5, 100, "Available");
+INSERT INTO CinemaRoom (Name, Capacity, State)
+VALUES ("Daisy", 100, "Available");
 
 COMMIT;
 -- --------------------------------------------------------
