a56b96461121cf47a0d0dbe3eeecd7eed1abeaff
Excel Chendrawan <92035860+XcelCh@users.noreply.github.com>
2023-05-21 16:16:17 +0800

    Update UpdateMovieSessionController.java

--- UpdateMovieSessionController.java
+++ UpdateMovieSessionController.java
@@ -2,36 +2,15 @@ package controller;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import entity.CinemaRoom;
 import entity.Movie;
 import entity.MovieSession;
 
 public class UpdateMovieSessionController {
 	private final MovieSession movieSession = new MovieSession();
 	private final Movie movie = new Movie();
-	private final CinemaRoom cinemaRoom = new CinemaRoom();
-	
-	private final String DATE_FORMAT = "yyyy-MM-dd";
-	
-	public boolean isDateValid(String date)
-	{
-		try {
-			DateFormat df = new SimpleDateFormat(DATE_FORMAT);
-			df.setLenient(false);
-			df.parse(date);
-			
-			return true;
-			
-		} catch (ParseException e) {
-			return false;
-		}
-	}
 	
 	public boolean checkMovieId(String input) throws SQLException, Exception {
 		ResultSet rs = movie.getMovieIdCheckData(input);
@@ -39,8 +18,11 @@ public class UpdateMovieSessionController {
 		return rs.next();
 	}
 	
-	public String executeTask(String roomID, String sessionTiming, String movieID) throws SQLException, Exception{
-		MovieSession newSession = new MovieSession(roomID, sessionTiming, movieID);
+	public String executeTask(String roomID, String sessionTiming, String newMovieID, String oldMovieID) throws SQLException, Exception{
+		movieSession.deleteSession(roomID, sessionTiming);
+		
+		MovieSession oldSession = new MovieSession(roomID, sessionTiming, oldMovieID);
+		MovieSession newSession = new MovieSession(roomID, sessionTiming, newMovieID);
 		
 		ResultSet[] rsArr = movieSession.getConflictCheckData(newSession);
 		
@@ -55,31 +37,39 @@ public class UpdateMovieSessionController {
 			LocalDateTime aft = LocalDateTime.parse(rsArr[1].getString("SessionTiming"), formatter);
 			
 			if(newData.isBefore(bef.plusMinutes(30 + Integer.parseInt(movie.retrieveMovie(rsArr[0].getString("MovieID"))[4]))) || 
-			   aft.isBefore(newData.plusMinutes(30 + Integer.parseInt(movie.retrieveMovie(movieID)[4]))))
+			   aft.isBefore(newData.plusMinutes(30 + Integer.parseInt(movie.retrieveMovie(newMovieID)[4])))) {
+				movieSession.addSession(oldSession);
+				
 				return "Conflicting Session";
-			else
+			} else {
 				return movieSession.addSession(newSession);
+			}
 			
 		} else if(rsArr[0].isBeforeFirst()) {
 			rsArr[0].next();
 			
 			LocalDateTime bef = LocalDateTime.parse(rsArr[0].getString("SessionTiming"), formatter);
 			
-			if(newData.isBefore(bef.plusMinutes(30 + Integer.parseInt(movie.retrieveMovie(rsArr[0].getString("MovieID"))[4]))))
+			if(newData.isBefore(bef.plusMinutes(30 + Integer.parseInt(movie.retrieveMovie(rsArr[0].getString("MovieID"))[4])))) {
+				movieSession.addSession(oldSession);
+				
 				return "Conflicting Session";
-			else
+			} else {
 				return movieSession.addSession(newSession);
+			}
 			
 		} else if(rsArr[1].isBeforeFirst()) {
 			rsArr[1].next();
 			
 			LocalDateTime aft = LocalDateTime.parse(rsArr[1].getString("SessionTiming"), formatter);
 			
-			if(aft.isBefore(newData.plusMinutes(30 + Integer.parseInt(movie.retrieveMovie(movieID)[4]))))
+			if(aft.isBefore(newData.plusMinutes(30 + Integer.parseInt(movie.retrieveMovie(newMovieID)[4])))) {
+				movieSession.addSession(oldSession);
+				
 				return "Conflicting Session";
-			else
+			} else {
 				return movieSession.addSession(newSession);
-			
+			}	
 		} else {
 			return movieSession.addSession(newSession);
 		}
